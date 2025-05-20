11ad3ac8adb6614fc1d21d69ee5965aa9b8374b5
Kim C Yu <kcyu1998@yahoo.com>
2025-05-19 22:34:45 -0400

    Initial Commit

---
+++ YouAreAwesomeTests/YouAreAwesomeTests.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeTests.swift
+//  YouAreAwesomeTests
+//
+//  Created by KIM YU on 5/19/25.
+//
+
+import Testing
+@testable import YouAreAwesome
+
+struct YouAreAwesomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
