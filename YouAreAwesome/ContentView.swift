5f2b0c8e89a2ba6f36227d9be1abce52f56f3d16
Kim C Yu <kcyu1998@yahoo.com>
2025-05-20 23:09:22 -0400

    Finished you are awesome app

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,19 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = "I am A Programmer!"
+    
     var body: some View {
         
+        
+        
         VStack {
+            Spacer()
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-            Text("I am an app deceloper!")
+                .frame(width: 200, height: 200)
+                
+            Text(message)
                 .font(.largeTitle)
                 .fontWeight(/*@START_MENU_TOKEN@*/.heavy/*@END_MENU_TOKEN@*/)
                 .foregroundColor(Color.purple)
+            Spacer()
+            HStack {
+                Button("Awesome") {
+                    message = "You are awesome!"
+                }
+            
+                
+                Button("Great!") {
+                    message = "You are Great!"
+                }
+                
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.largeTitle)
+            .fontWeight(.ultraLight)
+            .tint(.orange)
+            .cornerRadius(15)
         }
-        .padding()
+            .padding()
+            
     }
 }
 
