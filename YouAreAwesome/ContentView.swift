1fa7233918725c09fa7c98478944c76f86c0bdd6
Kim C Yu <kcyu1998@yahoo.com>
2025-05-23 10:49:27 -0400

    showing multiple images

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var message = ""
-    @State private var image = ""
+    @State private var imageName = ""
+    @State private var imageNum = -1
     var message0 = ""
     var body: some View {
         
@@ -17,11 +18,11 @@ struct ContentView: View {
         
         VStack {
             Spacer()
-            Image(image)
+            Image("image\(imageNum)")
                 .resizable()
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 30))
-            
+                .shadow(radius: 40)
             
             Text(message)
                 .font(.largeTitle)
@@ -29,21 +30,23 @@ struct ContentView: View {
                 .foregroundColor(Color.red)
             Spacer()
             
-            Button("Press Me!") {
+            Button("Show Message!") {
                 
                 let message1 = "You Are Awesome!"
                 let message2 = "You Are Great!"
-                let image1 = "image0"
-                let image2 = "image1"
+                
                
                 message = ( message == message1 ? message2 : message1)
-                image = ( image == image1 ? image2 : image1)
+  //              imageName = ( imageName == image1 ? image2 : image1)
+                
+          imageNum = (imageNum == 9 ? 0 : imageNum + 1)
+ //              if imageNum == 9 { imageNum = 0
+  //              } else{imageNum += 1}
                 
             }
             .buttonStyle(.borderedProminent)
-            .font(.largeTitle)
+            .font(.title)
             .fontWeight(.ultraLight)
-            .tint(.orange)
             .cornerRadius(15)
         }
         .padding()
