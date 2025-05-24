d956e147ec71963f6656da030f6a052184ee65a9
Kim C Yu <kcyu1998@yahoo.com>
2025-05-23 22:08:08 -0400

    Adding array of messages

---
+++ Array Playground.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+//var messages = ["You Are Awesome!", "You Are Amazing!", "Fabulous? That's You!"]
+//print(messages)
+//print(messages.count)
+//messages.insert( "You Are Amazing!", at: 0)
+//print(messages)
+//let removedValue = messages.remove(at: 2)
+//print(removedValue)
+//print(messages)
+
+//Empty Arrays
+var grades: [Double] = []
+grades.append(82)
+grades += [91, 88]
+print("")
+print(grades)
--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -10,38 +10,39 @@ import SwiftUI
 struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
-    @State private var imageNum = -1
-    var message0 = ""
+    @State private var imageNum = 0
+    @State private var messageNum = 0
+    var messages : [String] = ["Your are awesome!", "You are great!", "You are amazing!", "You are incredible!", "You are fantastic!", "You Make Me Smile", "When the Genius Bar Needs Help, They Call You!"]
     var body: some View {
         
         
         
         VStack {
             Spacer()
-            Image("image\(imageNum)")
+            Image(imageName)
                 .resizable()
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 30))
                 .shadow(radius: 40)
             
+            
             Text(message)
                 .font(.largeTitle)
                 .fontWeight(/*@START_MENU_TOKEN@*/.heavy/*@END_MENU_TOKEN@*/)
                 .foregroundColor(Color.red)
+                .multilineTextAlignment(.center)
             Spacer()
             
             Button("Show Message!") {
                 
-                let message1 = "You Are Awesome!"
-                let message2 = "You Are Great!"
                 
-               
-                message = ( message == message1 ? message2 : message1)
-  //              imageName = ( imageName == image1 ? image2 : image1)
+                message = messages[messageNum]
+                messageNum = (messageNum == messages.count-1 ? 0 : messageNum + 1)
                 
-          imageNum = (imageNum == 9 ? 0 : imageNum + 1)
- //              if imageNum == 9 { imageNum = 0
-  //              } else{imageNum += 1}
+    
+                imageName = "image\(imageNum)"
+                imageNum = (imageNum == 9 ? 0 : imageNum + 1)
+ 
                 
             }
             .buttonStyle(.borderedProminent)
